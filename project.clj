be4b4c06674dc2937ce5a5878211f7790c973505
debugger87 <yangchaozhong.2009@gmail.com>
2013-03-27 14:24:01 +0800

    fix

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.clojars.kriyative/clojurejs "1.2.20"
+(defproject org.clojars.wujiangcheng/clojurejs "1.2.20"
   :description "A naive Clojure to Javascript translator"
-  :url "http://github.com/kriyative/clojurejs"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :url "http://github.com/debugger87/clojurejs"
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-clojars "0.5.0"]
